8ec67aae6720edc52954b60bb728fd5d5c1aefe8
NMR <niumaoru@163.com>
2019-12-12 07:08:21 +0000

    管理后台 - 用户列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -11,5 +11,7 @@ Route::group([
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index')->name('admin.home');
+    //$router->get('users', 'UsersController@index');
+    $router->resource('users', UsersController::class);
 
 });
